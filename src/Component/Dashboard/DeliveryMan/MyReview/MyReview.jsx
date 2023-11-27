b1732e89817c1e06bd32303c035b4f65fa897fef
674RoMaN <tauhidroman@gmail.com>
2023-11-27 14:14:23 +0600

    delivered done

--- src/Component/Dashboard/DeliveryMan/MyReview/MyReview.jsx
+++ src/Component/Dashboard/DeliveryMan/MyReview/MyReview.jsx
@@ -6,7 +6,7 @@ const MyReview = () => {
     const axiosPublic = useAxiosPublic()
     const { user } = useAuth()
 
-    const { data: reviews = [], refetch } = useQuery({
+    const { data: reviews = [] } = useQuery({
         queryKey: ['reviews'],
         queryFn: async () => {
             const result = await axiosPublic.get(`/reviews?email=${user?.email}`)
@@ -29,7 +29,6 @@ const MyReview = () => {
                             <p className="mb-3 font-semibold text-black "> Rating given on : <span className="mb-3 font-normal text-gray-700 dark:text-gray-400">{review.reviewDate}</span> </p>
                             <p className="mb-3 font-semibold text-black "> Given Rating : <span className="mb-3 font-normal text-gray-700 dark:text-gray-400">{review.rating} out of 5.0</span> </p>
                             <p className="mb-3 font-medium text-black"> Given Review : <span className="mb-3 font-normal text-gray-700 dark:text-gray-400">{review.review}</span> </p>
-
                         </div>
                     </div>
                     )}
